b54349c22004ef973e87d00ab79e6ac1f1acf8d7
Nguyen Duong <nnbduong@gmail.com>
2021-06-23 18:20:45 +0700

    add protected route

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import * as ROUTES from './constants/routes';
 import useAuthListener from './hooks/use-auth-listener';
 import UserContext from './context/user';
 
+import ProtectedRoute from './helpers/protected-route';
+import IsUserLoggedIn from './helpers/is-user-logged-in';
+
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
 const NotFound = lazy(() => import('./pages/not-found'));
 const Dashboard = lazy(() => import('./pages/dashboard'));
+const Profile = lazy(() => import('./pages/profile'));
 
 function App() {
   const { user } = useAuthListener();
@@ -17,9 +21,18 @@ function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <IsUserLoggedIn path={ROUTES.LOGIN} loggedInPath={ROUTES.DASHBOARD} user={user}>
+              <Login />
+            </IsUserLoggedIn>
+            <IsUserLoggedIn path={ROUTES.SIGN_UP} loggedInPath={ROUTES.DASHBOARD} user={user}>
+              <SignUp />
+            </IsUserLoggedIn>
+            <Route path={ROUTES.PROFILE}>
+              <Profile />
+            </Route>
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard />
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
